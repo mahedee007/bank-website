9d3cddf954e91fb7da251fd2635d78f9fd3d0f10
Mahedee Hasan <mahedee007@gmail.com>
2023-07-30 01:57:41 +0600

    more function added

--- js/banking.js
+++ js/banking.js
@@ -1,67 +1,75 @@
-function getInput(inputId){
-    const inputAmount = document.getElementById(inputId);
-    const totalInputAmount = inputAmount.value;
-    const totalInput = parseFloat(totalInputAmount);
-    inputAmount.value = "";
-    return totalInput;
+// function getInput(inputId){
+//     const inputAmount = document.getElementById(inputId);
+//     const totalInputAmount = inputAmount.value;
+//     const totalInput = parseFloat(totalInputAmount);
+//     inputAmount.value = "";
+//     return totalInput;
 
-}
-function depositWithdraw(totalId, amount){
-    const depositWithdraw = document.getElementById(totalId);
-    const depositWithdrawText = depositWithdraw.innerText;
-    const depositWithdrawAmount = parseFloat(depositWithdrawText);
-    depositWithdraw.innerText = depositWithdrawAmount + amount;
+// }
+// function depositWithdraw(totalId, amount){
+//     const depositWithdraw = document.getElementById(totalId);
+//     const depositWithdrawText = depositWithdraw.innerText;
+//     const depositWithdrawAmount = parseFloat(depositWithdrawText);
+//     depositWithdraw.innerText = depositWithdrawAmount + amount;
     
-}
-function currentBalance(){
-    const balance = document.getElementById("balance");
-    const balanceText = balance.innerText;
-    const totalBalanceText = parseFloat(balanceText);
-    return totalBalanceText;
-}
+// }
+// function currentBalance(){
+//     const balance = document.getElementById("balance");
+//     const balanceText = balance.innerText;
+//     const totalBalanceText = parseFloat(balanceText);
+//     return totalBalanceText;
+// }
 
-function updateBalance(total, isAdd){
-    const balance = document.getElementById("balance");
-    // const balanceText = balance.innerText;
-    // const totalBalanceText = parseFloat(balanceText);
-    const totalBalanceText = currentBalance();
+// function updateBalance(total, isAdd){
+//     const balance = document.getElementById("balance");
+//     // const balanceText = balance.innerText;
+//     // const totalBalanceText = parseFloat(balanceText);
+//     const totalBalanceText = currentBalance();
     
-    if(isAdd === true ){
-        balance.innerText = totalBalanceText + total;
-    }else {
-        balance.innerText = totalBalanceText - total;
+//     if(isAdd === true ){
+//         balance.innerText = totalBalanceText + total;
+//     }else {
+//         balance.innerText = totalBalanceText - total;
 
-    }
+//     }
     
-}
+// }
 
 
-document.getElementById("deposit-btn").addEventListener("click", function(){
+// document.getElementById("deposit-btn").addEventListener("click", function(){
    
-   const totalDeposit = getInput("deposit-input");
-   if (totalDeposit > 0 ){
-    depositWithdraw("deposit", totalDeposit);
+//    const totalDeposit = getInput("deposit-input");
+//    if (totalDeposit > 0 ){
+//     depositWithdraw("deposit", totalDeposit);
     
-    updateBalance(totalDeposit, true);
+//     updateBalance(totalDeposit, true);
 
-   }else{
-       alert("please enter a valid number");
-   }
+//    }else{
+//        alert("please enter a valid number");
+//    }
     
     
 
-})
-document.getElementById("withdraw-btn").addEventListener("click", function(){
+// })
+// document.getElementById("withdraw-btn").addEventListener("click", function(){
     
-    const totalWithdraw = getInput("withdraw-input");
-    const myBalance = currentBalance();
-    if (totalWithdraw> 0 && totalWithdraw < myBalance){
-        depositWithdraw("withdraw", totalWithdraw);
+//     const totalWithdraw = getInput("withdraw-input");
+//     const myBalance = currentBalance();
+//     if (totalWithdraw> 0 && totalWithdraw < myBalance){
+//         depositWithdraw("withdraw", totalWithdraw);
 
-    updateBalance(totalWithdraw, false);
+//     updateBalance(totalWithdraw, false);
 
-    }else{
-        alert("please enter a valid number");
+//     }
 
     
+// })
+
+document.getElementById('deposit-btn').addEventListener('click', function(){
+  const inputField = document.getElementById('deposit-input');
+  const enterDeposit = inputField.value;
+  const enterDeposittNumber = parseFloat(enterDeposit);
+  const depositField = document.getElementById('deposit');
+  depositField.innerText = enterDeposit;
+  inputField.value = ''; 
 })
