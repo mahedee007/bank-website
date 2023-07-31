b22b4bb4dfa3ef744c6f96c847da702b1704cfb8
Mahedee Hasan <mahedee007@gmail.com>
2023-07-31 23:15:12 +0600

    deposit part done

--- js/banking.js
+++ js/banking.js
@@ -3,11 +3,18 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
   const inputField = document.getElementById('deposit-input');
   const enterDeposit = inputField.value;
-  const enterDeposittNumber = parseFloat(enterDeposit);
+  const enterDepositNumber = parseFloat(enterDeposit);
   const depositField = document.getElementById('deposit');
-  const depositFieldstring = parseFloat(depositField);
-  const newDeposit = depositFieldstring.innerText;
-  const totalDeposit = enterDeposittNumber + newDeposit;
+  const depositFieldstring = depositField.innerText;
+  const depositFieldNumber = parseFloat(depositFieldstring);
+  const newDepositTotal = depositFieldNumber + enterDepositNumber;
+  depositField.innerText = newDepositTotal;
+
+  const balance = document.getElementById('balance');
+  const balanceString = balance.innerText;
+  const balanceNumber = parseFloat(balanceString);
+  const totalBalance = balanceNumber + enterDepositNumber;
+  balance.innerText = totalBalance;
 
   inputField.value = ''; 
 })
